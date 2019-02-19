84bead9f2e52431430c779343fcb55009cbad59c
andrewiskang <andrewiskang@gmail.com>
2019-02-18 22:01:40 -0600

    working on looping gif, set shortest option to stop at shortest input

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # steps to creating final product:
 #  1. convert GIF and .mp3 into .mp4 file
+#     DONE
 #  2. loop GIF and .mp3 into .mp4 file
+#     IN PROGRESS
 #  3. analyze .mp3 file (using bpm), return results
 #  4. adjust speed of looping GIF based on .mp3 results
 #  5. convert adjusted, looped GIF and .mp3 into .mp4 file
@@ -11,13 +13,13 @@ import os
 
 def convertToVideo(gifPath, mp3Path, outPath):
     mp3 = ffmpeg.input(mp3Path)
-    gif = ffmpeg.input(gifPath)
+    gif = ffmpeg.input(gifPath, stream_loop=-1)
     a1 = mp3["a"]
     v1 = mp3["v"]
     (
         ffmpeg
-        .output(mp3, gif, outPath)
-        .run()
+        .output(mp3, gif, outPath, shortest=outPath)
+        .run() #overwrite_output=True
     )
 
 
@@ -31,7 +33,7 @@ for file in os.listdir("in/"):
         mp3Paths.append("in/" + file)
 
 counter = 0
-for gif in gifPaths:
-    for mp3 in mp3Paths:
-        convertToVideo(gif, mp3, "out/out"+str(counter)+".mp4")
+for gifPath in gifPaths:
+    for mp3Path in mp3Paths:
+        convertToVideo(gifPath, mp3Path, "out/out"+str(counter)+".mp4")
         counter += 1
